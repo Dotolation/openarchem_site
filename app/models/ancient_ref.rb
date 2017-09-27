c463ecc5cbaa23e8e4ee4c34faed8c12d41913d2
Anna Krohn <akkrohn@gmail.com>
2017-09-27 14:45:27 -0400

    allow for multiple ancient refs in the same modal

--- app/models/ancient_ref.rb
+++ app/models/ancient_ref.rb
@@ -2,7 +2,7 @@ class AncientRef < ActiveRecord::Base
 
   def self.find_by_prod_id(id)
     unless id.empty?
-      ref = AncientRef.where("product_id = ?", id).first
+      ref = AncientRef.where("product_id = ?", id)
     else
       return nil
     end
